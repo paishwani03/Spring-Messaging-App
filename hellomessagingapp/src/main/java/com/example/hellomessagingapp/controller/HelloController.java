6f76fd0af1d2439e1c252635f2466302e54b93a7
paishwani03 <120698480+paishwani03@users.noreply.github.com>
2025-02-28 17:58:40 +0530

    UC-2

---
+++ hellomessagingapp/src/main/java/com/example/hellomessagingapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hellomessagingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+    @GetMapping("/hello/query{name}")
+    public String sayHelloWithQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
