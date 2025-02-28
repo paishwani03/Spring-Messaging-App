f9b14ea91f6ef4e349d9533bad26c9be10a1c1fc
paishwani03 <120698480+paishwani03@users.noreply.github.com>
2025-02-28 19:51:52 +0530

    UC-4

---
+++ hellomessagingapp/src/main/java/com/example/hellomessagingapp/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.hellomessagingapp.controller;
+
+import com.example.hellomessagingapp.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHelloWithQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String sayHelloPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+}
